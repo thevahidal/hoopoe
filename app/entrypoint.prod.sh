0a60d92acd103163aa9b5fb23f3c5346804beddd
Vahid Al <thevahidal@gmail.com>
2022-01-12 01:29:58 +0330

    --noinput added to collect static

--- app/entrypoint.prod.sh
+++ app/entrypoint.prod.sh
@@ -2,12 +2,13 @@
 
 echo "Waiting for postgres..."
 while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-  sleep 0.1
+    echo $POSTGRES_HOST $POSTGRES_PORT
+    sleep 0.1
 done
 echo "PostgreSQL started"
 
 python manage.py migrate
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 
 python manage.py init_telegram_client
 echo "Telegram client started"
