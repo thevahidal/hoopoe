611b3acdb3b31b8b6a7679ae800e7192eb1794fc
Vahid Al <thevahidal@gmail.com>
2022-01-12 01:30:12 +0330

    static root added

--- app/hoopoe/settings.py
+++ app/hoopoe/settings.py
@@ -130,9 +130,14 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/staticfiles/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"),)
+
+MEDIA_URL = "/mediafiles/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
