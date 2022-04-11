54569faa54eec207725e8c65c9c691bb86fed52d
Vahid Al <thevahidal@gmail.com>
2022-04-11 13:31:16 +0430

    postgres up-check removed from entrypoint

--- app/entrypoint.prod.sh
+++ app/entrypoint.prod.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-echo "Waiting for postgres..."
-while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-    echo $POSTGRES_HOST $POSTGRES_PORT
-    sleep 0.1
-done
-echo "PostgreSQL started"
+# echo "Waiting for postgres..."
+# while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+#     echo $POSTGRES_HOST $POSTGRES_PORT
+#     sleep 0.1
+# done
+# echo "PostgreSQL started"
 
 python manage.py migrate
 python manage.py collectstatic --noinput
